71ee1f706bf2977df63e064c93236b14fd8d1297
sunrose3 <237639461@qq.com>
2023-04-01 18:04:20 +0800

    加了显示内容

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  link = 'https://github.com/sunrose3/keyboard';
   title = 'keyboard';
+
+  value = "";
+  value1 = "";
+  value2 = "";
+  value3 = "";
 
   onChange(string: string | number) {
     console.log(string);
+    this.value = this.value+string;
+  }
+  onChange1(string: string | number) {
+    console.log(string);
+    this.value1 += string;
+  }
+  onChange2(string: string | number) {
+    console.log(string);
+    this.value2 += string;
+  }
+  onChange3(string: string | number) {
+    console.log(string);
+    this.value3 += string;
   }
 }
